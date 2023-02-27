8bc0232a616bf51a07be0beaa497bcc6feaa8401
Lukasz Kaminski <44691382+Loleus@users.noreply.github.com>
2023-02-27 09:01:21 +0100

    api_component_connected

---
+++ public/components/blog/blog.js
@@ -0,0 +1,38 @@
+const content = (shadowRoot) => {
+    const fetchJson = async () => {
+        try {
+          let response = await fetch('/client/getAll');
+          let parsedList = await response.json();
+          return parsedList
+        } catch (err) {
+          console.error(err)
+        }
+      }
+      const output = ({ name }) => {
+        return `<li> ${name} </li>`
+      }
+      
+      const renderList = async (post) => {
+      
+          post.forEach(function (item) {
+            shadowRoot.getElementById("posts").innerHTML += output(item);
+          });
+      
+      }
+      const renderMenu = async () => {
+        let posts = [];
+        try {
+          let getList = await fetchJson();
+          for (let obj of getList) {
+            posts.push(obj)
+          }
+          console.log(posts)
+          renderList(posts);
+        } catch (err) {
+          console.error(err)
+        }
+      }
+      console.log("jeb")
+      renderMenu();
+}
+export default content
