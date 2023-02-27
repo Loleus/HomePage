481a1da439fdbe635be9bf4536a2cbf13db65f19
Loleus <loleus@gmail.com>
2023-02-27 05:41:14 +0100

    ss

--- public/components/aboutMe/index.js
+++ public/components/aboutMe/index.js
@@ -1,3 +1,4 @@
+import {html} from "/public/components/aboutMe/template.html"
 export default class About extends HTMLElement {
   static get observedAttributes() { return ["loading"]; }
   constructor() {
@@ -15,11 +16,9 @@ export default class About extends HTMLElement {
     const temp = await fetch("/components/aboutMe/template.html", { mode: 'cors' })
     const tempStream = await temp.text()
     this.base = tempStream;
-    this.tmp = document.createElement('template');
-    this.tmp.innerHTML += this.base
     this.loading = false;
   }
-  
+
   async connectedCallback() {
     this.shadowRoot.addEventListener("click", (e) => {
       console.log(e.target)
@@ -27,18 +26,20 @@ export default class About extends HTMLElement {
     await this.getCard();
     this.render();
   }
-  disconnectedCallback() { }
-
-  attributeChangedCallback(attrName, oldVal, newVal) {
-
+  htmlToElement(html) {
+    const temp = document.createElement('template');
+    html = html.trim();
+    temp.innerHTML += html;
+    return temp.content;
   }
   render() {
-console.log("tutej")
+    const { shadowRoot } = this;
+    const tmp = this.htmlToElement(html);
     if (this.loading) {
-      this.shadowRoot.innerHTML = `Loading...`;
+      shadowRoot.innerHTML = `Loading...`;
     } else {
-      this.shadowRoot.innerHTML = ``;
-      this.shadowRoot.appendChild(this.tmp.content.cloneNode(true));
+      shadowRoot.innerHTML = ``;
+      shadowRoot.appendChild(tmp.cloneNode(true));
     }
   }
 };
