60d4119dda56764c18bc439976b70cde030807c4
Lukasz Kaminski <44691382+Loleus@users.noreply.github.com>
2023-03-02 07:08:46 +0100

    asd

--- record/post-record.js
+++ record/post-record.js
@@ -1,36 +1,36 @@
 const {ValidationError} = require('../utils/errors')
-class ClientRecord {
+class PostRecord {
   constructor(obj) {
-      const {id, mail, name, nextContactAt, notes, category} = obj;
+      const {id, date, title, lastEdit, text, picUrl} = obj;
       if (!id || typeof id !== 'string') {
         throw new ValidationError('ID nie może być puste!');
       }
 
-      if(!name || typeof name !== 'string' || name.length < 3) {
+      if(!title || typeof title !== 'string' || title.length < 3) {
         throw new ValidationError('Imię musi być tekstem o długości min. 3 znaków.');
       }
 
-      if(!mail || typeof mail !== 'string' || mail.indexOf('@') === -1) {
-        throw new ValidationError('Email Nieprawidłowy!');
+      if(!date || typeof date !== 'string' || date.indexOf('@') === -1) {
+        throw new ValidationError('Edate Nieprawidłowy!');
       }
 
-      if (typeof nextContactAt !== 'string') {
+      if (typeof lastEdit !== 'string') {
         throw new ValidationError('Data następnego kontaktu musi być tekstem!')
       }
 
-      if (typeof notes !== 'string') {
+      if (typeof text !== 'string') {
         throw new ValidationError('Notatki muszą być tekstem!')
       }
 
       this.id = id;
-      this.name = name;
-      this.mail = mail;
-      this.nextContactAt = nextContactAt;
-      this.notes = notes;
-      this.category = category;
+      this.title = title;
+      this.date = date;
+      this.lastEdit = lastEdit;
+      this.text = text;
+      this.picUrl = picUrl;
   }
 }
 
 module.exports = {
-  ClientRecord,
+  PostRecord,
 }
