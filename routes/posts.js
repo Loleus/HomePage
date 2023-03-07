f764d35ded375d1a65d296b06d703d5b7755a2d1
Loleus <loleus@gmail.com>
2023-03-07 05:10:29 +0100

    cxcx

--- routes/posts.js
+++ routes/posts.js
@@ -5,12 +5,14 @@ const posts = require('../utils/posts');
 /* GET programming languages. */
 postsRouter.get('/', async function(req, res, next) {
   try {
-    res.json(await posts.getMultiple(req.query.page));
-    res.send(db.getAll())
+    // res.json(await posts.getMultiple(req.query.page));
+    res.send(await posts.getMultiple(req.query.page))
   } catch (err) {
     console.error(`Error while getting programming languages `, err.message);
     next(err);
   }
 });
 
-module.exports = postsRouter;
+module.exports = {
+  postsRouter
+};
