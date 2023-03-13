4d37afe6643ff075cb835a4da333acfc36974737
Loleus <loleus@gmail.com>
2023-03-13 20:59:58 +0100

    delete_old

--- front/js/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./home.js";
-import AboutUs from "./about.js";
-import NotFound from "./notfound.js";
-import Users from "./users.js";
-import UserDetails from "./userdetails.js";
-
-export { Home, AboutUs, NotFound, Users, UserDetails };
