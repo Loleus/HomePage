62b724c3f01c36052869190d06b4c2f2077b8d67
Lukasz Kaminski <44691382+Loleus@users.noreply.github.com>
2023-03-17 12:36:04 +0100

    ff

--- index.js
+++ index.js
@@ -52,7 +52,33 @@ app.post('/login', (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
   if (username == 'admin' && password == 'admin') {
-    // Execute SQL query that'll select the account from the database based on the specified username and password
+//     CREATE DATABASE IF NOT EXISTS `nodelogin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+// USE `nodelogin`;
+
+// CREATE TABLE IF NOT EXISTS `accounts` (
+//   `id` int(11) NOT NULL AUTO_INCREMENT,
+//   `username` varchar(50) NOT NULL,
+//   `password` varchar(255) NOT NULL,
+//   `email` varchar(100) NOT NULL,
+//   PRIMARY KEY (`id`)
+// ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+// INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
+    // connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		// 	// If there is an issue with the query, output the error
+		// 	if (error) throw error;
+		// 	// If the account exists
+		// 	if (results.length > 0) {
+		// 		// Authenticate the user
+		// 		request.session.loggedin = true;
+		// 		request.session.username = username;
+		// 		// Redirect to home page
+		// 		response.redirect('/home');
+		// 	} else {
+		// 		response.send('Incorrect Username and/or Password!');
+		// 	}			
+		// 	response.end();
+		// });
     req.session.loggedin = true;
     req.session.username = username;
     console.log("loggedIn")
