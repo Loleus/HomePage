e192c47b3a5b38698426d57d21f1c8a2dcf8bafd
Loleus <loleus@gmail.com>
2023-03-25 23:44:26 +0100

    exif_config

--- public/pages/userdetails.js
+++ public/pages/userdetails.js
@@ -1,7 +1,7 @@
 let post
 const userList = async (id) => {
   try {
-    let response = await fetch('/client/'+id);
+    let response = await fetch('/client/' + id);
     let parsedList = await response.json();
     post = parsedList
   } catch (err) {
@@ -19,21 +19,28 @@ export default class UserDetails extends HTMLElement {
     const id = this.getAttribute("id");
     await userList(id)
     console.log(id)
-  
+
     if (id && id !== null) {
-      this.innerHTML = `
-        <div class="page">
-          <h1 class="title">${post.title}</h1>
-          <p class="container">${post.text}</p>
-          <img style="height:67vh;display:none" src="https://drive.google.com/uc?id=${post.picUrl}" alt="blogpic"/>
-        </div>
-        <form style="margin-bottom:20px;" class="addForm">
-        <button type="button" onclick="javascript:history.back()">Back to posts</button>
-        </form>
-      `;
+      const temp = document.getElementById("blog_temp");
+      const div = temp.content.children[1]
+      console.log(div)
+      let cont = temp.content.cloneNode(true);
+      this.appendChild(cont)
+      document.getElementById('title').innerHTML = post.title
+      document.getElementById('text').innerHTML = post.text
+      const image = document.getElementById('img1');
+      const getExif = () => {
+        window.EXIF.getData(image, function () {
+          var MetaData = window.EXIF.getAllTags(this);
+          console.log(JSON.stringify(MetaData, null, "\t"));
+        });
+      }
+      image.src = `http://drive.google.com/uc?id=${post.picUrl}`;
+      image.onload = () => {
+
+        image.style = "height:67vh;display:inline-block";
     }
-    document.querySelector('img').onload = () => {
-      document.querySelector('img').style = "height:67vh;display:inline-block"
+            getExif();
     };
   }
 }
