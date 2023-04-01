14ef881fba70097ffa6992a4fed34f2fc19e2e03
Loleus <loleus@gmail.com>
2023-04-01 21:30:02 +0200

    delete_spans

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const session = require('express-session');
 const path = require('path');
 const sessionObj = require('./utils/session');
 const router = require('./utils/router');
+
 const app = express()
+
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sessionObj()));
+
 router(app);
+
 app.listen(3000, '0.0.0.0', () => {
   console.log("Listening on http://0.0.0.0:3000")
 });
