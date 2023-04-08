de35649926622aedf90f4a85c11815fa86134f6d
Loleus <loleus@gmail.com>
2023-04-08 18:34:39 +0200

    ready_to_render

--- src/utils/session.js
+++ src/utils/session.js
@@ -1,13 +1,15 @@
-module.exports = function() {
+module.exports = function(memoryStore) {
   return {
       secret: 'secret',
       resave: true,
       saveUninitialized: true,
+      store: memoryStore,
       cookie: {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true, 
         secure: false,
         sameSite: 'strict',
+
     }
   }
 }
