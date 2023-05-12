8737753b0382c1c47545d32248e803d873a8eea8
Loleus <loleus@gmail.com>
2023-05-12 05:45:27 +0200

    kgskdg

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -9,18 +9,17 @@ import Login from "/components/login/index.js";
 import AddPhoto from "/components/addPhoto/index.js";
 import EditPhoto from "/components/editPhoto/index.js";
 import Photos from "/components/photos/index.js";
-import Photo from "/components/photo/index.js";
 import NotFound from "/components/errors/index.js";
 import Menu from "/components/menu/index.js";
 import Spinner from "/components/spinner/index.js";
 import Card from "/components/photos_card/index.js";
 
 const routes = [
-  ["wc-router", Router], ["wc-home", Home], ["wc-repos", Repos], ["wc-about", AboutMe], ["wc-music", Music], ["wc-video", Video], ["wc-contact", Contact], ["wc-login", Login], ["wc-addphoto", AddPhoto], ["wc-editphoto", EditPhoto], ["wc-photos", Photos], ["wc-photo", Photo], ["wc-notfound", NotFound],["wc-spinner", Spinner],["wc-card", Card]
+  ["wc-router", Router], ["wc-home", Home], ["wc-repos", Repos], ["wc-about", AboutMe], ["wc-music", Music], ["wc-video", Video], ["wc-contact", Contact], ["wc-login", Login], ["wc-addphoto", AddPhoto], ["wc-editphoto", EditPhoto], ["wc-photos", Photos], ["wc-notfound", NotFound],["wc-spinner", Spinner],["wc-card", Card]
 ];
 
 for (let route of routes) {
   customElements.define(route[0],route[1])
 };
 
-export { Router, Home, Repos, AboutMe, Music, Video, Contact, Login, AddPhoto, EditPhoto, Photos, Photo, NotFound, Menu,  Spinner, Card};
+export { Router, Home, Repos, AboutMe, Music, Video, Contact, Login, AddPhoto, EditPhoto, Photos, NotFound, Menu,  Spinner, Card};
