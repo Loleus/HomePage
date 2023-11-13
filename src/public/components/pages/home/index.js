7071c23c74c6cf54e54159626e910823a1eb69d1
Loleus <loleus@gmail.com>
2023-11-13 21:39:59 +0100

    fgg

--- src/public/components/pages/home/index.js
+++ src/public/components/pages/home/index.js
@@ -1,19 +1,33 @@
-export default class Home extends HTMLElement {
-
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  };
-
-  async connectedCallback() {
-    this.render();
-  };
-
-  render() {
-    this.shadowRoot.innerHTML = `
-      <link rel="stylesheet prefetch" href="/components/pages/home/style.css">
-      <div id="fsm_container" class="fsm-container">
-      </div>
-    `
+const setup = async () => {
+  const resp = await fetch("/components/pages/home/template.html", { mode: 'cors' });
+  const html = await resp.text();
+  const base = html;
+  return class Home extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" })
+    }
+    async connectedCallback() {
+      this.render();
+    };
+  
+    htmlToElement(html) {
+      const temp = document.createElement('template');
+      temp.innerHTML += html;
+      return temp.content;
+    };
+  
+    render() {
+      const { shadowRoot } = this;
+      const tmp = this.htmlToElement(base);
+      if (this.loading) {
+        shadowRoot.innerHTML = `Loading...`;
+      } else {
+        shadowRoot.innerHTML = ``;
+        shadowRoot.appendChild(tmp.cloneNode(true));
+      }
+    };
   };
 };
+
+export default await setup();
