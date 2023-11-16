9df3e75e53d99b3af7cfe465809c6e97c9bc0283
Loleus <loleus@gmail.com>
2023-11-16 05:26:46 +0100

    fgd

--- src/public/components/pages/home/index.js
+++ src/public/components/pages/home/index.js
@@ -1,32 +1,25 @@
 const setup = async () => {
-  const resp = await fetch("/components/pages/home/template.html", { mode: 'cors' });
+  const resp = await fetch("/components/pages/home/template.html", {mode: "cors",});
   const html = await resp.text();
   const base = html;
+
+  let htmlToElement = () => {
+    const temp = document.createElement("template");
+    temp.innerHTML += base;
+    return temp.content;
+  };
+
   return class Home extends HTMLElement {
     constructor() {
       super();
-      this.attachShadow({ mode: "open" })
+      this.attachShadow({ mode: "open" });
     }
+
     async connectedCallback() {
-      this.render();
-    };
-  
-    htmlToElement(html) {
-      const temp = document.createElement('template');
-      temp.innerHTML += html;
-      return temp.content;
-    };
-  
-    render() {
-      const { shadowRoot } = this;
-      const tmp = this.htmlToElement(base);
-      if (this.loading) {
-        shadowRoot.innerHTML = `Loading...`;
-      } else {
-        shadowRoot.innerHTML = ``;
-        shadowRoot.appendChild(tmp.cloneNode(true));
-      }
-    };
+      const tmp = htmlToElement(base);
+      this.shadowRoot.innerHTML = ``;
+      this.shadowRoot.appendChild(tmp.cloneNode(true));
+    }
   };
 };
 
