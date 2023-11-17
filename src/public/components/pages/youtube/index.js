de26a185dc541d7a64c4703357dc03cb5e45a024
Lukasz Kaminski <44691382+Loleus@users.noreply.github.com>
2023-11-17 10:43:42 +0100

    dsf

--- src/public/components/pages/youtube/index.js
+++ src/public/components/pages/youtube/index.js
@@ -1,29 +1,30 @@
-export default class Youtube extends HTMLElement {
+import { initer } from "/.././components/vendors/loader/index.js";
 
-  async connectedCallback() {
-    this.tmp = this.htmlToElement();
-    this.render();
-  }
+const index = (tmp) => {
+  return class Youtube extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+    }
 
-  htmlToElement() {
-    const temp = document.createElement('template');
-    temp.innerHTML += `
-      <link rel="stylesheet" href="/components/pages/youtube/style.css">
-      <iframe id="player" type="text/html" frameborder="0"></iframe>
-    `;
-    const playerI = temp.content.querySelector('#player');
-    playerI.setAttribute('src', "https://www.youtube.com/embed?listType=playlist&list=PLkXJmTe_aZnZncsAHK4LgkP6kkt-ataG3");
-    
-    return temp.content;
-  }
-
-  render() {
-    this.innerHTML = `<wc-spinner></wc-spinner>`;
-    this.appendChild(this.tmp.cloneNode(true));
-    player.style.visibility = "hidden"
-    player.addEventListener('load', (e) => {
-      player.style.visibility = "visible";
-      this.removeChild(this.firstElementChild)
-  })
-  }
+    async connectedCallback() {
+      this.shadowRoot.innerHTML = ``;
+      this.shadowRoot.appendChild(tmp.cloneNode(true));
+      const scs = this.shadowRoot.getElementById('player');
+      scs.style.visibility = "hidden";
+      scs.setAttribute('src', "https://www.youtube.com/embed?listType=playlist&list=PLkXJmTe_aZnZncsAHK4LgkP6kkt-ataG3")
+      scs.addEventListener("load", (e) => {
+        scs.style.visibility = "visible";
+        this.shadowRoot.removeChild(this.shadowRoot.firstElementChild);
+      });
+    }
+  };
 };
+
+const name = "youtube";
+
+const setup = async () => {
+  return initer(index, name);
+};
+
+export default await setup();
